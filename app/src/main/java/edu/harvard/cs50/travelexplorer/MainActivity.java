09073d8ab75c3a6efed8ec4d2f6876b505880947
kcyl9 <kristyphone119@gmail.com>
2020-12-24 23:01:15 -0500

    Added regions list

--- app/src/main/java/edu/harvard/cs50/travelexplorer/MainActivity.java
+++ app/src/main/java/edu/harvard/cs50/travelexplorer/MainActivity.java
@@ -1,14 +1,34 @@
 package edu.harvard.cs50.travelexplorer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    List<String> regionsList = Arrays.asList("Africa", "Antartic", "Asia", "Central America", "Middle East", "North America", "Oceania", "South America", "The Caribbean");
+    List<String> regions = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        regions.addAll(regionsList);
+
+        linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        RegionAdapter regionAdapter = new RegionAdapter((ArrayList<String>) regions, MainActivity.this);
+        recyclerView.setAdapter(regionAdapter);
     }
 }
---
+++ app/src/main/java/edu/harvard/cs50/travelexplorer/RegionAdapter.java
@@ -0,0 +1,61 @@
+package edu.harvard.cs50.travelexplorer;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegionAdapter extends RecyclerView.Adapter {
+
+    ArrayList regions;
+    Context context;
+
+    public RegionAdapter(ArrayList<String> regions, Context context) {
+        this.regions = regions;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.region_row, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+
+        ViewHolder regionViewHolder = (ViewHolder) holder;
+        regionViewHolder.region.setText((CharSequence) regions.get(position));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), (CharSequence) regions.get(position),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return regions.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView region;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            region = itemView.findViewById(R.id.region_row_text_view);
+        }
+    }
+}
